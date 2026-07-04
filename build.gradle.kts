aa4ee8c0c26aa4d6ff651707521889bb526d3881
Neha-2021 <nehakumari01012001@gmail.com>
2026-07-04 20:55:17 +0530

    add spotless for formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.16"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.diffplug.spotless") version "7.2.1"
 }
 
 group = "orchestrator"
@@ -32,4 +33,13 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+spotless {
+	java {
+		googleJavaFormat("1.27.0")
+		removeUnusedImports()
+		trimTrailingWhitespace()
+		endWithNewline()
+	}
 }
