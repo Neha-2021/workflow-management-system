15c4d6017e6bc4aab6695a92a01fd9a1f8081612
Neha-2021 <nehakumari01012001@gmail.com>
2026-07-04 20:53:49 +0530

    add getters and setter in BaseEntity

--- src/main/java/orchestrator/common/entity/BaseEntity.java
+++ src/main/java/orchestrator/common/entity/BaseEntity.java
@@ -1,16 +1,25 @@
 package orchestrator.common.entity;
 
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Version;
-
+import jakarta.persistence.Version;
 import java.util.UUID;
 
 @MappedSuperclass
 public abstract class BaseEntity {
-    @Id
-    private UUID id;
+  @Id private UUID id;
 
-    @Version
-    private Long version;
+  @Version private Long version;
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public Long getVersion() {
+    return version;
+  }
 }
