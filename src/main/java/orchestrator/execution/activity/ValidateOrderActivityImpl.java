02c433bd61dea735b0aee9a4a15d2f434aaa8a71
Neha-2021 <nehakumari01012001@gmail.com>
2026-07-13 17:44:03 +0530

    add component annotation

--- src/main/java/orchestrator/execution/activity/ValidateOrderActivityImpl.java
+++ src/main/java/orchestrator/execution/activity/ValidateOrderActivityImpl.java
@@ -1,7 +1,9 @@
 package orchestrator.execution.activity;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ValidateOrderActivityImpl implements Activity {
   @Override
   public String getName() {
